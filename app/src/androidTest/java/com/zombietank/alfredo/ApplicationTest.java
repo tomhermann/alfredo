204e3cba47cabe1a23313f6e771e2553605b1297
Tom Hermann <tomhermann@gmail.com>
2016-07-24 09:36:43 -0700

    Initial commit, empty project + retrolambda.

---
+++ app/src/androidTest/java/com/zombietank/alfredo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.zombietank.alfredo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
